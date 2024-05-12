a334e8369ff6fd22a9822de11a9caaeab2c150ce
Rashidzada <rashidzada6@gmail.com>
2024-05-12 19:22:01 +0500

    I have only updated the settings.py for the +static(settings.MEDIA_URL)

--- Book_lib/urls.py
+++ Book_lib/urls.py
@@ -2,11 +2,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static , settings
-admin.site.site_header = "Book - Lib"
-admin.site.site_title = "Book - Lib"
+admin.site.site_header = "Book - Lib WebApp"
+admin.site.site_title = "Book - Lib WebApp"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-]
-urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
